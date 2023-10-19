ab2456057792382b3824af4c9a1b14894ea43356
MEKuroko007 <essalhimu@gmail.com>
2023-10-19 14:02:49 +0200

    quick sort with fix betty style and doc

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,47 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order using
+ * insertion_sort_list - Sorts a doubly linked list
+ * of integers in ascending order using
  * the Insertion sort algorithm.
  * @list: A pointer to the head of the list.
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        temp = current;
+	while (current)
+	{
+		temp = current;
 
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            if (temp->next != NULL)
-                temp->next->prev = temp->prev;
-            temp->prev->next = temp->next;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
 
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
 
-            if (temp->prev != NULL)
-                temp->prev->next = temp;
-            else
-                *list = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
 
-            if (temp->next != NULL)
-                temp->next->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = temp;
 
-            if (temp->prev == NULL)
-                current = temp;
+			if (temp->prev == NULL)
+				current = temp;
 
-            print_list(*list);
-        }
+			print_list(*list);
+		}
 
-        current = current->next;
-    }
+		current = current->next;
+	}
 }
---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * @array: The array to be sorted
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ *
+ * Return: The partition index
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int temp, j, temp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[i + 1] != array[high])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+/**
+ * quicksort - Sorts an array of integers using Quick sort
+ * @array: The array to be sorted
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int result;
+
+	if (low < high)
+	{
+		result = lomuto_partition(array, low, high, size);
+		quicksort(array, low, result - 1, size);
+		quicksort(array, result + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
+}
